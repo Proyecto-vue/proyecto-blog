312dc844fb861c8732e637a2b701fb8608a77d71
Brenedin <brenedingomez@protonmail.com>
2020-07-10 15:24:52 -0600

    desarrollo de pagina principal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,104 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div id="nav">
+    <nav>
+      <a id="nombre">Andrea</a>
+      <a id="fotoperfil"></a>
+      <a id="mas">+</a>
+      <a id="home">Home</a>
+      <a id="blogs">Blogs</a>
+    </nav>
+    <br />
+
+    <aside id="aside">
+      <nav>
+        <a id="asideblog1"></a>
+      </nav>
+    </aside>
+    <div id="principal"></div>
   </div>
 </template>
 
@@ -113,18 +29,132 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#principal {
+  position: absolute;
+  width: 1121px;
+  height: 659px;
+  left: 24px;
+  top: 105px;
+  margin: auto;
+  background: #000000;
+  mix-blend-mode: normal;
+  border-radius: 15px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/* Rectangle 1 */
+#nav {
+  position: absolute;
+  width: 100%;
+  height: 22px;
+  left: 0px;
+  top: 0px;
+  margin: auto;
+  background: #000000;
+  mix-blend-mode: normal;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#aside {
+  position: absolute;
+  width: 231px;
+  height: 853px;
+  left: 1188px;
+  top: 105px;
+  margin: auto;
+  background: #000000;
+  mix-blend-mode: normal;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
 }
-a {
-  color: #42b983;
+
+/* Home */
+#home {
+  position: absolute;
+  width: 97px;
+  height: 42px;
+  left: 1205px;
+  top: 21px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+
+  color: #ffed48;
+}
+
+/* Andrea */
+#nombre {
+  position: absolute;
+  width: 116px;
+  height: 42px;
+  left: 39px;
+  top: 21px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+
+  color: #ffffff;
+}
+#fotoperfil {
+  position: absolute;
+  width: 74px;
+  height: 69px;
+  left: 170px;
+  top: 8px;
+
+  background: #ffed48;
+  border-radius: 360px;
+}
+/* Blogs */
+#blogs {
+  position: absolute;
+  width: 91px;
+  height: 84px;
+  left: 1337px;
+  top: 21px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+
+  color: #ffed48;
+}
+
+/* + */
+#mas {
+  position: absolute;
+  width: 34px;
+  height: 39px;
+  left: 1145px;
+  top: 15px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 48px;
+  line-height: 56px;
+
+  color: #ffed48;
+
+  mix-blend-mode: normal;
+}
+
+#asideblog1 {
+  position: absolute;
+  width: 208px;
+  height: 187px;
+  left: 1200px;
+  top: 117px;
+
+  background: #ffffff;
+  mix-blend-mode: normal;
+  border-radius: 15px;
 }
 </style>
+
