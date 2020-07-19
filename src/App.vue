aec8b4c52527b61e54661c13beb322ae59084618
Brenedin <brenedingomez@protonmail.com>
2020-07-19 01:37:26 -0600

    cambie diseno de home, carta

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,51 @@
         <h3>BlogName</h3>
       </div>
       <div class="nav-wrap">
-    <ul class="nav nav-pills">
-  <li class="nav-item">
-    <router-link class="nav-link" to="/">Home</router-link>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Pages</a>
-    <div class="dropdown-menu">
-     <router-link class="dropdown-item" to="/blog/create">Blog Create</router-link>
-     <router-link class="dropdown-item" to="/blog/show">Blog Grid</router-link>
-    </div>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">User</a>
-    <div class="dropdown-menu">
-     <router-link class="dropdown-item" to="/login">
-               <!-- Door Opening for LOGIN -->
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-door-open" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M1 15.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM11.5 2H11V1h.5A1.5 1.5 0 0 1 13 2.5V15h-1V2.5a.5.5 0 0 0-.5-.5z"/>
-                  <path fill-rule="evenodd" d="M10.828.122A.5.5 0 0 1 11 .5V15h-1V1.077l-6 .857V15H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117z"/>
-                  <path d="M8 9c0 .552.224 1 .5 1s.5-.448.5-1-.224-1-.5-1-.5.448-.5 1z"/>
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Pages</a>
+            <div class="dropdown-menu">
+              <router-link class="dropdown-item" to="/blog/create">Blog Create</router-link>
+              <router-link class="dropdown-item" to="/blog/show">Blog Grid</router-link>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">User</a>
+            <div class="dropdown-menu">
+              <router-link class="dropdown-item" to="/login">
+                <!-- Door Opening for LOGIN -->
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  class="bi bi-door-open"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 15.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM11.5 2H11V1h.5A1.5 1.5 0 0 1 13 2.5V15h-1V2.5a.5.5 0 0 0-.5-.5z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.828.122A.5.5 0 0 1 11 .5V15h-1V1.077l-6 .857V15H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117z"
+                  />
+                  <path d="M8 9c0 .552.224 1 .5 1s.5-.448.5-1-.224-1-.5-1-.5.448-.5 1z" />
                 </svg>
-     </router-link>
-     
-    </div>
-  </li>
-  <li class="nav-item">
-     <router-link class="nav-link" to="/about">About</router-link>
-  </li>
- 
-</ul>
+              </router-link>
+            </div>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
       </div>
     </div>
-    
-    <router-view />
 
+    <router-view />
   </div>
 </template>
 
@@ -50,28 +60,26 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #f5f5f5;
+  background-color: #ffffff;
   min-height: 100vh;
 }
 
-.divNAV{
+.divNAV {
   box-shadow: 0 2px 4px #bbbbbb50;
 }
 
-@media(max-width:450px){
-  .divNAV{
-  flex-direction: column;
-  justify-content: center;
+@media (max-width: 450px) {
+  .divNAV {
+    flex-direction: column;
+    justify-content: center;
 
-  .wp-title{
-    margin: 0 auto;
-  }
+    .wp-title {
+      margin: 0 auto;
+    }
 
-  .nav-wrap{
-    margin: 0 auto;
+    .nav-wrap {
+      margin: 0 auto;
+    }
   }
 }
-}
-
-
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <div id="nav">
-      <nav>
+    <!--  <div id="nav">
+      <nav> 
         <a id="nombre">Andrea</a>
         <a class="fotoperfil"></a>
         <a id="mas">+</a>
         <a id="home">Home</a>
         <a id="blogs">Blogs</a>
       </nav>
-    </div>
+    </div>-->
 
     <div class="aside">
       <div class="asidebox">
         <a class="fotoaside"></a>
         <a class="textoaside">Carla</a>
-        <p class="textoaside2">Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+        <p class="textoaside2"></p>
       </div>
       <div class="asidebox"></div>
       <div class="asidebox"></div>
@@ -22,7 +22,13 @@
       <div class="asidebox"></div>
     </div>
 
-    <div id="principal"></div>
+    <div id="principal">
+      <div class="liston"></div>
+      <h1 class="fuente2">Tu titulo</h1>
+    </div>
+    <div class="comments">
+      <h1 class="fuente1">Commments</h1>
+    </div>
   </div>
 </template>
 
@@ -37,17 +43,39 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Sora:wght@300&display=swap");
 #principal[data-v-469af010] {
   position: absolute;
-  width: 82%;
-  height: 100%;
-  left: 1%;
+  width: 79%;
+  height: 70%;
+  left: 3%;
   top: 13%;
   margin: auto;
-  background: #000000;
+  background: #ffffff;
   mix-blend-mode: normal;
-  border-radius: 15px;
+  /*border-radius: 15px; */
   box-shadow: 4px 4px 18px rgba(0, 0, 0, 0.5);
+}
+
+.liston {
+  background: #ecececff;
+  width: 100%;
+  height: 5%;
+  top: 0%;
+  background-image: url("../assets/rect833.png");
+}
+
+.comments {
+  position: absolute;
+  width: 79%;
+  height: 30%;
+  left: 3%;
+  top: 90%;
+  margin: auto;
+  background: #ffffff;
+  mix-blend-mode: normal;
+  /*border-radius: 15px; */
+  box-shadow: 2px 2px 9px rgba(0, 0, 0, 0.3);
 }
 
 /* Rectangle 1 */
@@ -63,16 +91,16 @@ export default {
 }
 .aside[data-v-469af010] {
   position: absolute;
-  width: 15%;
+  width: 13%;
   height: 100%;
-  left: 84%;
+  left: 86%;
   top: 13%;
   margin: auto;
   background: #ffffff;
   mix-blend-mode: normal;
   display: flex;
   flex-direction: column;
-  border-radius: 15px;
+  /* border-radius: 15px; */
   box-shadow: 8px 8px 18px #c4c4c4, -8px -8px 16px #ffffff;
 }
 .asidebox {
@@ -85,7 +113,7 @@ export default {
   background: #ffffff;
   border-style: solid;
   mix-blend-mode: normal;
-  border-radius: 15px;
+  /*border-radius: 15px; */
 }
 
 /* Home */
@@ -210,5 +238,13 @@ export default {
 
   color: #000000;
 }
-</style>
 
+.fuente1 {
+  font-family: "Sora", sans-serif;
+  font-size: 1.5rem;
+}
+.fuente2 {
+  font-family: "Sora", sans-serif;
+  font-size: 2rem;
+}
+</style>
